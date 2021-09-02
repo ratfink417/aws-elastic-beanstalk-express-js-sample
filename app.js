ce23dd878f62026cc7652c5846e03b0c9d7302ae
ratfink417 <ratfink417@gmail.com>
2021-09-02 10:46:01 -0700

    Testing the build pipeline manual approval

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello, build pipeline!'));
+app.get('/', (req, res) => res.send('TEST TEST TEST'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
